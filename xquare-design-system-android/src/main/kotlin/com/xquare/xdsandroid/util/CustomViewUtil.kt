f0bb92f29b172a0407957615d467ce89b9d1a066
Tmdhoon2 <hks026naver@gmail.com>
2023-08-23 08:15:58 +0900

    ⚡️ :: set isEnabled

--- xquare-design-system-android/src/main/kotlin/com/xquare/xdsandroid/util/CustomViewUtil.kt
+++ xquare-design-system-android/src/main/kotlin/com/xquare/xdsandroid/util/CustomViewUtil.kt
@@ -4,6 +4,7 @@ import android.view.View
 import com.xquare.xdsandroid.ViewDefaults
 
 fun View.setAlphaEnabled(isEnabled: Boolean) {
+    this.isEnabled = isEnabled
     if (!isEnabled) {
         this.alpha = ViewDefaults.ALPHA_DISABLED
     }
